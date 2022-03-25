d94a7fee8f1cb740de06c7c5c62a4f12e95e0b2e
Philip Rydqvist <90709110+PhilipRydqvist@users.noreply.github.com>
2022-03-25 10:11:08 +0100

    Second commit
    about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+
+import About from './views/About';
+
 function App() {
   return (
     <div className="App">
       <p>testa</p>
+
+      <Routes>
+        <Route path="/about" element = { <About/> } />
+  
+      </Routes>
     </div>
   );
 }
