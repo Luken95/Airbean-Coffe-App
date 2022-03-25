4bbcc84cc7d838c6728a6160303c738ca16b705f
Luken95 <Lukass_basket@hotmail.com>
2022-03-25 10:07:30 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-import Status from './views/Status'
+import Status from './views/Status';
 import Landing from './views/Landing';
+import Menylista from './views/Menylista';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
      <Routes>
       <Route path="/status" element={ <Status /> } />
       <Route path='/landing' element={ <Landing /> }></Route>
+      <Route path='/menu' element={ <Menylista /> } />
      </Routes>
     </div>
   );
--- src/views/Menylista.js
+++ src/views/Menylista.js
@@ -1,11 +1,36 @@
+import {useState, useEffect} from 'react';
+
+
 function Menylista (){
+
+    const [ Menu, setMenu ] = useState('');
+
+    async function getMenu(){
+      const response = await fetch(`https://my-json-server.typicode.com/zocom-christoffer-wallenberg/airbean/menu`)
+      const data = await response.json();
+  
+      console.log(data);
+      setMenu(data);
+    }
+  
+  
+    useEffect(() => {
+      getMenu();
+    }, []);
+  
+
 
 return(
 <div>
 <h1>Meny</h1>
 
+
+
+
 </div>
 )
 }
+
+export default Menylista
 
 
