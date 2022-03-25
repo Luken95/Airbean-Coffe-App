9f5724afee7aff879fc9bef26631e8e7a6078282
Twedmark <axel.twedmark@hotmail.com>
2022-03-25 09:36:03 +0100

    added /status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+
+import Status from './views/Status'
 
 function App() {
   return (
     <div className="App">
-      <p>testa</p>
+    <Routes>
+      <Route path="/status" element={ <Status /> } />
+    </Routes>
     </div>
   );
 }
