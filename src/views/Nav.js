17b1bc4f8555600a0f21e3417ae757533d3c887c
Luken95 <Lukass_basket@hotmail.com>
2022-03-26 19:01:24 +0100

    update navbar

--- src/views/Nav.js
+++ src/views/Nav.js
@@ -7,14 +7,17 @@ function Nav (){
 return(
     
 <section className='Navbar'>
-
 <Link className='Links' to="/menu" style={{ textDecoration: 'none' }}> Meny </Link>
 <br></br>
+<div className="dash"></div>
 <br></br>
 <Link className='Links' to="/about" style={{ textDecoration: 'none' }}> Vårt kaffe </Link>
 <br></br>
+<div className="dash"></div>
 <br></br>
 <Link className='Links' to="/status" style={{ textDecoration: 'none' }}> Orderstatus </Link>
+<br></br>
+<div className="dash"></div>
 
 </section>
 )
