cd5f2fc7cbfd298971614dd11dccb6e57bb91c1c
Luken95 <Lukass_basket@hotmail.com>
2022-04-05 10:45:28 +0200

    update

--- src/views/LogIn.js
+++ src/views/LogIn.js
@@ -4,12 +4,17 @@ import './LogIn.css';
 function Login (){
 
     return(
-        <section className="login">
+        <section >
             <Header />
-            <h1>Logga in</h1>
+            <form className="login">
+            <h1>Välkommen till AirBean-familjen!</h1>
+            <p>Genom att skapa ett konto nedan kan du spara och se din orderhistorik.</p>
+            <label>Namn</label>
             <input type="text" className="userName"></input>
+            <label>Lösenord</label>
             <input type="text" className="password"></input>
             <button type="button" className="logInButton">Logga in</button>
+            </form>
         </section>
 
     )
