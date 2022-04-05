31081d9850c4ff5877f7c27bf22e4e270e377e0d
Niklas Johansson <niklasdjohansson@gmail.com>
2022-04-05 10:39:51 +0200

    databas

---
+++ src/database/operations.js
@@ -0,0 +1,53 @@
+const nedb = require('nedb-promise');
+const database = new nedb({ filename: 'users.db', autoload: true});
+
+const users = {
+    'type': 'users',
+    'all-users': [
+        {
+            "id":1,
+            username :"Anders",
+            "email":"anders@anders.se",
+            orderhistorik: {
+
+            }           
+        },
+        {
+            "id":1,
+            username:"Bosse",
+            "email":"bosse@bosse.se",
+            orderhistorik: {
+
+            }           
+        },
+        {
+            "id":1,
+            username:"Christoffer",
+            "email":"christoffer@christoffer.se",
+            orderhistorik: {
+
+            }           
+        },
+    ]
+}
+
+async function saveUsers() {
+    database.insert(users)
+}
+
+async function findUser(username) {
+    return await database.findOne({ username: username})
+}
+
+async function addOrder(username, order) {
+    database.update({
+        username: username 
+        },{
+        $push: {
+            orderhistorik: order
+        }
+
+    })
+}
+
+module.exports = {saveUsers, findUser, addOrder}
